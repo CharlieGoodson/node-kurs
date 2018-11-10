b496a1c5e239dd8078e183bbc09fec7b487e2222
CharlieGoodson <max.sadchikov@gmail.com>
2018-11-10 19:06:34 +0200

    added engines and pug templates

--- routes/admin.js
+++ routes/admin.js
@@ -2,15 +2,19 @@ const express = require('express')
 const path = require('path')
 const rootDir = require('./../util/path')
 
+const products = []
+
 const router = express.Router()
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    res.render('add-product', {docTitle: 'Add Product'})
 })
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body.title)
+    products.push({title: req.body.title})
     res.redirect('/')
 })
 
-module.exports = router
+module.exports.routes = router
+module.exports.products = products
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const path = require('path')
 const rootDir = require('./../util/path')
+const adminData = require('./admin')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    // console.log(adminData.products)
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    res.render('shop', {
+        products: adminData.products,
+        docTitle: 'Shop'
+    })
 })
 
 module.exports = router
