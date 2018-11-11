97ab8937c1a85173038c6e540ebbecf9c4fd13eb
CharlieGoodson <max.sadchikov@gmail.com>
2018-11-11 23:24:29 +0200

    xx

--- app.js
+++ app.js
@@ -30,5 +30,5 @@ app.use((req, res, next) => {
     })
 })
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server started on port ${port}`))
