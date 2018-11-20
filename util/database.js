fb868c0c13044d1220fdba5f88f2e0f33230920e
CharlieGoodson <max.sadchikov@gmail.com>
2018-11-20 13:49:48 +0200

    xx

--- util/database.js
+++ util/database.js
@@ -1,19 +1,21 @@
 const Sequelize = require('sequelize')
 
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
 // const sequelize = new Sequelize('node-complete', 'root', 'root', {
 //             dialect: 'mysql',
 //             host: 'localhost'
 // })
 
-let sequelize = null
+// let sequelize = null
 
-if (!process.env.NODE_ENV) {
-    sequelize = new Sequelize('node-complete', 'root', 'root', {
-        dialect: 'mysql',
-        host: 'localhost'
-    })
-} else {
-    sequelize = new Sequelize(process.env.DATABASE_URL)
-}
+// if (!process.env.NODE_ENV) {
+//     sequelize = new Sequelize('node-complete', 'root', 'root', {
+//         dialect: 'mysql',
+//         host: 'localhost'
+//     })
+// } else {
+//     sequelize = new Sequelize(process.env.DATABASE_URL)
+// }
 
 module.exports = sequelize
